b4459109392f4dea1a1773a658c5450814f02d75
Damien Maillard <damien.maillard@dailymotion.com>
2019-03-28 18:56:21 +0100

    update jsenv-core dependency and prefer import.meta.require

--- test/check-format.test.js
+++ test/check-format.test.js
@@ -1,9 +1,10 @@
-import path from "path"
 import { hrefToPathname } from "/node_modules/@jsenv/module-resolution/index.js"
 import { assert } from "/node_modules/@dmail/assert/index.js"
 import { checkFormat } from "../index.js"
 
-const projectFolder = path.resolve(hrefToPathname(import.meta.url), "../../")
+const { resolve } = import.meta.require("path")
+
+const projectFolder = resolve(hrefToPathname(import.meta.url), "../../")
 const filenameRelativeArray = [`index.js`]
 const actual = await checkFormat({ folder: projectFolder, filenameRelativeArray })
 const expected = {
